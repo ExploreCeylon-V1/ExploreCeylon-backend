22d396deb4c71d793d092b3f320f16bf3350284d
Dhanesh Ganearachchi <dhanesh.lakshan.it@gmail.com>
2026-06-23 22:04:37 +0530

    add destination review part
    created destination reviews part ,now user can put reviews for each destination

---
+++ src/main/java/com/exploreceylon/backend/repository/Destinationreviewrepository.java
@@ -0,0 +1,21 @@
+package com.exploreceylon.backend.repository;
+
+import com.exploreceylon.backend.model.DestinationReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+
+public interface Destinationreviewrepository extends JpaRepository<DestinationReview, Long> {
+
+    // GET /api/v1/destinations/{destinationId}/reviews
+    List<DestinationReview> findByDestination_IdOrderByCreatedAtDesc(Long destinationId);
+
+    @Query("SELECT AVG(r.rating) FROM DestinationReview r WHERE r.destination.id = :destinationId")
+    Double findAverageRatingByDestinationId(@Param("destinationId") Long destinationId);
+
+    @Query("SELECT COUNT(r) FROM DestinationReview r WHERE r.destination.id = :destinationId")
+    Integer countByDestinationId(@Param("destinationId") Long destinationId);
+}
